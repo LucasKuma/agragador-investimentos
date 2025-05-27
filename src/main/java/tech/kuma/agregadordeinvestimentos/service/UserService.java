326f185226617a93231cda6678fe1db6aa948fef
Lucas Kuhlmann <lucas.khmn@gmail.com>
2025-05-27 11:14:42 -0300

    User created

---
+++ src/main/java/tech/kuma/agregadordeinvestimentos/service/UserService.java
@@ -0,0 +1,80 @@
+package tech.kuma.agregadordeinvestimentos.service;
+
+import org.hibernate.sql.Update;
+import org.springframework.stereotype.Service;
+import tech.kuma.agregadordeinvestimentos.controller.CreateUserDto;
+import tech.kuma.agregadordeinvestimentos.controller.UpdateUserDto;
+import tech.kuma.agregadordeinvestimentos.entity.User;
+import tech.kuma.agregadordeinvestimentos.repository.UserRepository;
+
+import javax.swing.text.html.Option;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UUID createUser(CreateUserDto createUserDto) {
+
+        //DTO -> ENTITY
+        var entity = new User(
+                UUID.randomUUID(),
+                createUserDto.username(),
+                createUserDto.email(),
+                createUserDto.password(),
+                Instant.now(),
+                null
+        );
+
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserId();
+    }
+
+    public Optional<User> getUserById(String userId) {
+        return userRepository.findById(UUID.fromString(userId));
+    }
+
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    public void updateUserById (String userId, UpdateUserDto updateUserDto) {
+        var id = UUID.fromString(userId);
+
+        var userEntity = userRepository.findById(id);
+
+        if(userEntity.isPresent()) {
+            var user = userEntity.get();
+
+            if (updateUserDto.username() != null) {
+                user.setUsername(updateUserDto.username());
+            }
+
+            if (updateUserDto.password() != null) {
+                user.setPassword(updateUserDto.password());
+            }
+
+            userRepository.save(user);
+        }
+    }
+
+    public void deleteById(String userId) {
+
+        var id = UUID.fromString(userId);
+
+        var userExists = userRepository.existsById(id);
+
+        if (userExists) {
+            userRepository.deleteById(id);
+        }
+    }
+}
---
+++ src/test/java/tech/kuma/agregadordeinvestimentos/AgregadordeinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kuma.agregadordeinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadordeinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
